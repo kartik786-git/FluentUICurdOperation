faa59bfb91ab9aa0351929f0ec298efa5d4b587b
kartik <kartik.kumar53@gmail.com>
2023-12-26 18:03:06 +0530

    Add project files.

---
+++ FluentUICurdOperation/Data/Blog/IBlogService.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace FluentUICurdOperation.Data.Blog
+{
+    public interface IBlogService
+    {
+        Task<List<Blog>> GetAllAsync();
+        Task<Blog> GetByIdAsync(int id);
+        Task<Blog> CreateAsync(Blog blog);
+        Task<int> UpdateAsync(int id, Blog blog);
+        Task<int> DeleteAsync(int id);
+
+    }
+}
